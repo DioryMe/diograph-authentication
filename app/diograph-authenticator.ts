faf91f6899584daed16e2c8fc1fa97a0871cbe73
Jouni Alanen <jouni@diory.me>
2018-07-07 00:01:52 +0300

    Sketching out as comments + DiographAuthenticator.isAuthenticated() created

--- app/diograph-authenticator.ts
+++ app/diograph-authenticator.ts
@@ -1,27 +1,45 @@
 export class DiographAuthenticator {
 
-  static login(loginInfo) {
+  static isAuthenticated() {
+    // return true if valid cookie exists
+    // return false if invalid or missing cookie
+  }
 
+  static login(loginInfo) {
+    // Return "already logged in" if isAuthenticated
+
+    // Try to retrieve master token with loginInfo
+
+    // Success
+    // - create cookie
+    // - save master token to cookie
+
+    // Failure
+    // - return error message
   }
 
   static logout() {
-
+    // Delete cookie
   }
 
   static getToken(type) {
+    // If !isAuthenticated, return always null
 
+    // Read everything from cookie
     switch(type) {
       case "all": {
-        return "all"
+        // let tokens = {}
+        // let tokenNames = ["diograph-server", "google-maps"]
+        // tokens.forEach((name) => {
+        //   tokens[name] = this.getToken(name)
+        // })
+        // return tokens
       }
       case "diograph-server": {
-
+        // return DiographServerTokenManager.getToken()
       }
       case "google-maps": {
-
-      }
-      default: {
-        console.log(type)
+        // return GoogleMapsTokenManager.getToken()
       }
     }
 
