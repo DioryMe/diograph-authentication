f46b55f2f2db71aefafa9fb1c8e881a79f7610b5
Jouni Alanen <jouni@diory.me>
2018-07-07 22:02:52 +0300

    DiographAuthenticatorSpec sketched out

---
+++ spec/diographAuthenticatorSpec.ts
@@ -0,0 +1,67 @@
+import { DiographAuthenticator } from '../app/diograph-authenticator'
+
+describe('DiographAuthenticator', () => {
+
+  beforeEach(() => {
+    // Mock cookie writer
+  })
+
+  describe('isAuthenticated()', () => {
+    it('returns true', () => {
+      expect(DiographAuthenticator.isAuthenticated()).toBe(true)
+    })
+  })
+
+  describe('login()', () => {
+
+    describe('with correct login info', () => {
+      beforeEach(() => {
+        // Mock sending login info to server
+        // Returns success
+      });
+
+      it('should create a cookie', () => {
+        // Calls cookie creation function
+      });
+    })
+
+    describe('with incorrect login info', () => {
+      beforeEach(() => {
+        // Mock sending login info to server
+        // Returns failure
+      });
+    })
+
+  })
+
+  describe('logout()', () => {
+    it('removes cookie', () => {
+      // Calls cookie removing function
+    });
+
+    it('clears tokens', () => {
+
+    });
+  })
+
+  describe('getToken()', () => {
+    describe('when not authenticated', () => {
+      it('returns null for everything', () => {
+        // all
+        // google-maps
+        // master-token
+      });
+    });
+
+    describe('when authenticated', () => {
+      it('with "all" calls all the retrievers one by one', () => {
+
+      });
+
+      it('with "google-maps" calls google-map retriever', () => {
+
+      });
+    });
+  })
+
+})
