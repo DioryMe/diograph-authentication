b537cb12bc23ee754ba01a778047a64255b631d4
Jouni Alanen <jvalanen@gmail.com>
2018-11-22 22:27:57 +0200

    Remove out commented code

--- app/local-storage-manager.ts
+++ app/local-storage-manager.ts
@@ -78,35 +78,3 @@ export class LocalStorageMock {
 }
 
 
-//       let inputFieldValue = (<HTMLInputElement>document.getElementById("diograph-token-input")).value;
-//       localStorage.setItem("token", inputFieldValue);
-//       localStorage.setItem("endpoint", DiographAuthentication.isProduction ? "http://diory-server.herokuapp.com/v1/" : "http://localhost:3000/v1/");
-//       self.readAndRefreshToken();
-//       (<HTMLInputElement>document.getElementById("diograph-token-input")).value = "";
-//       DiographAuthentication.onLogin();
-//     });
-
-//     document.getElementById("logout").addEventListener('click', () => {
-//       localStorage.removeItem("token");
-//       DiographAuthentication.onLogout();
-//     });
-
-
-
-//   beforeEach(() => {
-//     var store = {};
-
-//     spyOn(localStorage, 'getItem').and.callFake( (key:string):String => {
-//      return store[key] || null;
-//     });
-//     spyOn(localStorage, 'removeItem').and.callFake((key:string):void =>  {
-//       delete store[key];
-//     });
-//     spyOn(localStorage, 'setItem').and.callFake((key:string, value:string):string =>  {
-//       return store[key] = <string>value;
-//     });
-//     spyOn(localStorage, 'clear').and.callFake(() =>  {
-//         store = {};
-//     });
-// });
-
